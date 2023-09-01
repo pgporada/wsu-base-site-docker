ecd804c53302c1b05211703eb21ec5ffccc824aa
Chris Pelzer <chrispelzer@gmail.com>
2023-09-01 15:00:37 -0400

    move to individual docker php builds

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker build -t wsu-base-container .
+if [[ -z ${1} || -z ${2} ]]; then
+    echo "Usage: ${0} <type> <php-version>"
+    echo "type: base, extensions"
+    echo "php-version: 8.2.7"
+    exit 1
+fi
+
+docker build --no-cache -f docker/php-${2}/Dockerfile.${1} -t wsu-php-${2}-${1} .
