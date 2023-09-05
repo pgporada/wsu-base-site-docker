409e0afca895742a6d5813812d7b7873e32f8064
Phil Porada <philporada@gmail.com>
2023-09-05 11:13:50 -0400

    Quote vars

--- build.sh
+++ build.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
+set -e
 
 phpVersions=("5.5.38" "7.3.33" "8.0.30" "8.2.7")
 types=("base" "extensions" "config")
 
-argType=$1
-argPHPVersion=$2
+argType="${1}"
+argPHPVersion="${2}"
 
-if [[ -z ${1} || -z ${2} ]]; then
+if [[ -z "${1}" || -z "${2}" ]]; then
     echo "Usage: ${0} <type> <php-version>"
     echo "type: base, extensions, config, all"
     echo "php-version: 5.5.38, 7.3.33, 8.0.30, 8.2.7, all"
     exit 1
 fi
 
-build() {
-    type=$1
-    version=$2
-    docker build --no-cache -f ./docker/php-$version/$type.Dockerfile -t wsu-php-$version-$type .
-    docker tag wsu-php-$version-$type waynestate/php-$type:$version
-    docker push waynestate/php-$type:$version
+function build() {
+    if [[ -z "${1}" || -z "${2}" ]]; then
+        echo "You must supply a 'type' and a 'php version' as arguments."
+        return 1
+    fi
+    local type="${1}"
+    local version="${2}"
+
+    docker build --no-cache -f "./docker/php-${version}/${type}.Dockerfile" -t "wsu-php-${version}-${type}" .
+    docker tag "wsu-php-${version}-${type}" "waynestate/php-${type}:${version}"
+    docker push "waynestate/php-${type}:${version}"
 }
 
-if [[ "$argType" == "all" && "$argPHPVersion" == "all" ]]; then
+if [[ "${argType}" == "all" && "${argPHPVersion}" == "all" ]]; then
   for version in "${phpVersions[@]}"; do
       for type in "${types[@]}"; do
-          build $type $version
+          build "${type}" "${version}"
       done
   done
-elif [ "$argType" == "all" ]; then
+elif [ "${argType}" == "all" ]; then
   for type in "${types[@]}"; do
-      build $type $argPHPVersion
+      build "${type}" "${argPHPVersion}"
   done
-elif [ "$argPHPVersion" == "all" ]; then
+elif [ "${argPHPVersion}" == "all" ]; then
   for version in "${phpVersions[@]}"; do
-        build $argType $version
+        build "${argType}" "${version}"
   done
 else
-    build $argType $argPHPVersion
+    build "${argType}" "${argPHPVersion}"
 fi
